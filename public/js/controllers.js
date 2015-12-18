ce9e84e94de132d3917efe1d904202573c3eee88
eformat <eformat@gmail.com>
2015-12-19 12:15:42 +1300

    updated relative urls

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -1,91 +1,91 @@
 function navigateTOC() {
-	$("#embed").attr('data', "../public/0.%20Table%20of%20Contents.md.html");
-	//window.location.href = "../public/0.%20Setting%20up%20client%20tools.md.html";
+	$("#embed").attr('data', "0.%20Table%20of%20Contents.md.html");
+	//window.location.href = "0.%20Setting%20up%20client%20tools.md.html";
 }
 
 function navigate0() {
-	$("#embed").attr('data', "../public/0.%20Setting%20up%20client%20tools.md.html");
-    //window.location.href = "../public/0.%20Setting%20up%20client%20tools.md.html";
+	$("#embed").attr('data', "0.%20Setting%20up%20client%20tools.md.html");
+    //window.location.href = "0.%20Setting%20up%20client%20tools.md.html";
 }
 
 function navigate1() {
-	$("#embed").attr('data', "../public/1.%20Create%20App%20From%20a%20Docker%20Image.md.html");
-	//window.location.href = "../public/1.%20Create%20App%20From%20a%20Docker%20Image.md.html";
+	$("#embed").attr('data', "1.%20Create%20App%20From%20a%20Docker%20Image.md.html");
+	//window.location.href = "1.%20Create%20App%20From%20a%20Docker%20Image.md.html";
 }
     
 function navigate2() {
-	$("#embed").attr('data',"../public/2.%20Create%20App%20Using%20Docker%20Build.md.html");
-	// window.location.href = "../public/2.%20Create%20App%20Using%20Docker%20Build.md.html";
+	$("#embed").attr('data',"2.%20Create%20App%20Using%20Docker%20Build.md.html");
+	// window.location.href = "2.%20Create%20App%20Using%20Docker%20Build.md.html";
 }
 
 function navigate3() {
-	$("#embed").attr('data',"../public/3.%20Using%20Web%20Console.md.html");
-    //window.location.href = "../public/3.%20Using%20Web%20Console.md.html";
+	$("#embed").attr('data',"3.%20Using%20Web%20Console.md.html");
+    //window.location.href = "3.%20Using%20Web%20Console.md.html";
 }
     
 function navigate4() {
-	$("#embed").attr('data',"../public/4.%20Creating%20an%20application%20using%20JBoss%20EAP%20builder%20image.md.html");
-    //window.location.href = "../public/4.%20Creating%20an%20application%20using%20JBoss%20EAP%20builder%20image.md.html";
+	$("#embed").attr('data',"4.%20Creating%20an%20application%20using%20JBoss%20EAP%20builder%20image.md.html");
+    //window.location.href = "4.%20Creating%20an%20application%20using%20JBoss%20EAP%20builder%20image.md.html";
 }
 
 function navigate5() {
-	$("#embed").attr('data',"../public/5.%20Using%20templates.md.html");
-    //window.location.href = "../public/5.%20Using%20templates.md.html";
+	$("#embed").attr('data',"5.%20Using%20templates.md.html");
+    //window.location.href = "5.%20Using%20templates.md.html";
 }
     
 function navigate6() {
-	$("#embed").attr('data',"../public/6.%20Scale%20up%20and%20Scale%20down%20the%20application%20instances.md.html");
-    //window.location.href = "../public/6.%20Scale%20up%20and%20Scale%20down%20the%20application%20instances.md.html";
+	$("#embed").attr('data',"6.%20Scale%20up%20and%20Scale%20down%20the%20application%20instances.md.html");
+    //window.location.href = "6.%20Scale%20up%20and%20Scale%20down%20the%20application%20instances.md.html";
 }
 
 function navigate7() {
-	$("#embed").attr('data',"../public/7.%20Binary%20Deployment%20of%20a%20war%20file.md.html");
-    //window.location.href = "../public/7.%20Binary%20Deployment%20of%20a%20war%20file.md.html";
+	$("#embed").attr('data',"7.%20Binary%20Deployment%20of%20a%20war%20file.md.html");
+    //window.location.href = "7.%20Binary%20Deployment%20of%20a%20war%20file.md.html";
 }
     
 function navigate8() {
-	$("#embed").attr('data', "../public/8.%20Using%20SSL%20In%20your%20Application.md.html");
-    //window.location.href = "../public/8.%20Using%20SSL%20In%20your%20Application.md.html";
+	$("#embed").attr('data', "8.%20Using%20SSL%20In%20your%20Application.md.html");
+    //window.location.href = "8.%20Using%20SSL%20In%20your%20Application.md.html";
 }
 
 function navigate9() {
-	$("#embed").attr('data',"../public/9.%20Blue%20Green%20Deployments.md.html");
-	//window.location.href = "../public/9.%20Blue%20Green%20Deployments.md.html";
+	$("#embed").attr('data',"9.%20Blue%20Green%20Deployments.md.html");
+	//window.location.href = "9.%20Blue%20Green%20Deployments.md.html";
 }
 
 function navigate10() {
-	$("#embed").attr('data',"../public/10.%20SCM%20Web%20Hooks.md.html");
-    //window.location.href = "../public/10.%20SCM%20Web%20Hooks.md.html";
+	$("#embed").attr('data',"10.%20SCM%20Web%20Hooks.md.html");
+    //window.location.href = "10.%20SCM%20Web%20Hooks.md.html";
 }
 
 function navigate11() {
-	$("#embed").attr('data',"../public/11.%20Rollback%20Applications.md.html");
-    //window.location.href = "../public/11.%20Rollback%20Applications.md.html";
+	$("#embed").attr('data',"11.%20Rollback%20Applications.md.html");
+    //window.location.href = "11.%20Rollback%20Applications.md.html";
 }
     
 function navigate12() {
-	$("#embed").attr('data',"../public/12.%20Code%20Promotion%20Across%20Environments.md.html");
-    //window.location.href = "../public/12.%20Code%20Promotion%20Across%20Environments.md.html";
+	$("#embed").attr('data',"12.%20Code%20Promotion%20Across%20Environments.md.html");
+    //window.location.href = "12.%20Code%20Promotion%20Across%20Environments.md.html";
 }
 
 function navigate13() {
-	$("#embed").attr('data',"../public/13.%20Using%20JBoss%20Development%20Studio.md.html");
-    //window.location.href = "../public/13.%20Using%20JBoss%20Development%20Studio.md.html";
+	$("#embed").attr('data',"13.%20Using%20JBoss%20Development%20Studio.md.html");
+    //window.location.href = "13.%20Using%20JBoss%20Development%20Studio.md.html";
 }
 
 function navigate14() {
-	$("#embed").attr('data',"../public/14.%20Using%20Fabric8%20Maven%20Archetypes.md.html");
-    //window.location.href = "../public/14.%20Using%20Fabric8%20Maven%20Archetypes.md.html";
+	$("#embed").attr('data',"14.%20Using%20Fabric8%20Maven%20Archetypes.md.html");
+    //window.location.href = "14.%20Using%20Fabric8%20Maven%20Archetypes.md.html";
 }
 
 function navigate15() {
-	$("#embed").attr('data',"../public/15.%20A%20Build%20Pipeline%20Using%20Jenkins.md.html");
-    //window.location.href = "../public/15.%20A%20Build%20Pipeline%20Using%20Jenkins.md.html";
+	$("#embed").attr('data',"15.%20A%20Build%20Pipeline%20Using%20Jenkins.md.html");
+    //window.location.href = "15.%20A%20Build%20Pipeline%20Using%20Jenkins.md.html";
 }
 
 function navigate20() {
-	$("#embed").attr('data',"../public/20.%20Links.md.html");
-    //window.location.href = "../public/20.%20Links.md.html";
+	$("#embed").attr('data',"20.%20Links.md.html");
+    //window.location.href = "20.%20Links.md.html";
 }
 
 $("#menu-toc").click(function () {
